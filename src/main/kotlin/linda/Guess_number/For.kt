5271794e01fce99c63c495fab534dba2d219cb1a
DannyLien <danny.lien1109@gmail.com>
2020-08-11 14:31:06 +0800

    2020/8/11 For

---
+++ src/main/kotlin/linda/Guess_number/For.kt
@@ -0,0 +1,21 @@
+package linda.Guess_number
+
+import java.util.*
+
+fun main() {
+    val secret=Random().nextInt(10)+1
+    val scanner = Scanner(System.`in`)
+    for (i in 1..4) {
+        print("please enter a number (${i}/4):")
+        var number=scanner.nextInt()
+        println("第${i}次 $number")
+        if (number>secret){
+            println("lower")
+        }else if (number<secret){
+            println("higher")
+        }else{
+            println("Great,the number is $number")
+            break
+        }
+    }
+}
